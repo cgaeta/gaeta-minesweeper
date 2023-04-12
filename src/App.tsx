db6e6c27159186a9d1a4796b0231a00aa96a6213
Chris Gaeta <chrismgaeta@gmail.com>
2023-04-12 11:01:42 -0400

    clear empty cell groups and color numbers

--- src/App.tsx
+++ src/App.tsx
@@ -179,18 +179,104 @@ const shuffle = <T,>(array: T[]) => {
   }
 };
 
+const getProximateCells = (gameBoard: string[][], i: number, j: number) =>
+  gameBoard
+    .slice(Math.max(0, i - 1), Math.min(gameBoard.length + 1, i + 2))
+    .map((row) =>
+      row.slice(Math.max(0, j - 1), Math.min(row.length + 1, j + 2))
+    );
+
+const collectProximateEmptyCells = (
+  gameBoard: string[][],
+  i: number,
+  j: number,
+  clickedCells: Set<string>
+) => {
+  console.log(`current coordinates: ${i}-${j}`);
+  clickedCells.add(`${i}-${j}`);
+
+  if (
+    getProximateCells(gameBoard, i, j)
+      .flat()
+      .every((c) => c !== "!")
+  ) {
+    for (
+      let y = Math.max(0, i - 1);
+      y < Math.min(i + 2, gameBoard.length);
+      y++
+    ) {
+      for (
+        let x = Math.max(0, j - 1);
+        x < Math.min(j + 2, gameBoard[i].length);
+        x++
+      ) {
+        console.log(`check ${y}-${x}`, clickedCells.has(`${y}-${x}`));
+        if (!clickedCells.has(`${y}-${x}`)) {
+          collectProximateEmptyCells(gameBoard, y, x, clickedCells);
+        }
+      }
+    }
+  }
+
+  return clickedCells;
+};
+
+const CellNumber = ({ number }: { number: number }) => {
+  if (number === 0) {
+    return null;
+  }
+
+  if (number === 1) {
+    return <span className="font-semibold text-blue-800">1</span>;
+  }
+
+  if (number === 2) {
+    return <span className="font-semibold text-green-700">2</span>;
+  }
+
+  if (number === 3) {
+    return <span className="font-semibold text-red-600">3</span>;
+  }
+
+  if (number === 4) {
+    return <span className="font-semibold text-indigo-900">4</span>;
+  }
+
+  if (number === 5) {
+    return <span className="font-semibold text-yellow-900">5</span>;
+  }
+
+  if (number === 6) {
+    return <span className="font-semibold text-green-700">6</span>;
+  }
+
+  if (number === 7) {
+    return <span className="font-semibold text-gray-800">7</span>;
+  }
+
+  if (number === 8) {
+    return <span className="font-semibold text-gray-700">8</span>;
+  }
+
+  return null;
+};
+
 const Cell = ({
   gameBoard,
+  clickedCells,
+  setClickedCells,
   i,
   j,
   children = null,
 }: {
   gameBoard: string[][];
+  clickedCells: Set<string>;
+  setClickedCells: (set: Set<string>) => void;
   i: number;
   j: number;
   children: ReactNode;
 }) => {
-  const [clicked, setClicked] = useState("unclicked");
+  const clicked = clickedCells.has(`${i}-${j}`);
   const isMine = gameBoard[i][j] === "!";
   const proximateMines = useMemo(() => {
     return gameBoard
@@ -200,26 +286,27 @@ const Cell = ({
       );
   }, [gameBoard, i, j]);
 
-  if (clicked === "clicked")
+  if (clicked)
     return isMine ? (
       <button className="block border-box p-0 w-8 h-8 bg-red-600"></button>
     ) : (
-      <button
-        className="block border-box p-0 text-sm w-8 h-8 bg-gray-500"
-        onClick={() => setClicked("clicked")}
-      >
-        {proximateMines.flat().filter((c) => c === "!").length}
+      <button className="block text-base border-box p-0 text-sm w-8 h-8 bg-gray-400">
+        <CellNumber
+          number={proximateMines.flat().filter((c) => c === "!").length}
+        />
       </button>
     );
 
   return (
     <button
       className="block border-box p-0 text-sm w-8 h-8 border-4 bg-gray-400 border-gray-200 border-r-gray-600 border-b-gray-600"
-      onClick={() => setClicked("clicked")}
+      onClick={() =>
+        setClickedCells(
+          new Set(collectProximateEmptyCells(gameBoard, i, j, clickedCells))
+        )
+      }
     >
-      {clicked === "unclicked" && children}
-      {clicked === "clicked" &&
-        proximateMines.flat().filter((c) => c === "!").length}
+      {children}
     </button>
   );
 };
@@ -227,10 +314,9 @@ const Cell = ({
 function App() {
   const [settings, setSettings] = useState({ width: 9, height: 9, mines: 10 });
   const [gameStarted, setGameStarted] = useState(false);
+  const [clickedCells, setClickedCells] = useState(new Set<string>());
   const gameBoard = useMemo(() => {
     if (gameStarted) {
-      console.log(new Array(settings.height));
-
       const size = settings.width * settings.height;
       let board = new Array(settings.mines)
         .fill("!")
@@ -249,17 +335,25 @@ function App() {
     <div className="App">
       {gameStarted ? (
         <table>
-          {gameBoard.map((row, i) => (
-            <tr key={i}>
-              {row.map((cell, j) => (
-                <td key={`${i}-${j}`}>
-                  <Cell gameBoard={gameBoard} i={i} j={j}>
-                    {cell}
-                  </Cell>
-                </td>
-              ))}
-            </tr>
-          ))}
+          <tbody>
+            {gameBoard.map((row, i) => (
+              <tr key={i}>
+                {row.map((cell, j) => (
+                  <td key={`${i}-${j}`}>
+                    <Cell
+                      clickedCells={clickedCells}
+                      setClickedCells={setClickedCells}
+                      gameBoard={gameBoard}
+                      i={i}
+                      j={j}
+                    >
+                      {cell}
+                    </Cell>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
         </table>
       ) : (
         <Settings saveSettings={setSettings} setGameStarted={setGameStarted} />
